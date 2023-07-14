98b02ef82f74a0fcc454060d34611d9d8bc05ffa
deflationz <95709937+deflationz@users.noreply.github.com>
2023-07-14 00:36:45 -0400

    Create dpg_login_sys.py

---
+++ dpg_login_sys.py
@@ -0,0 +1,28 @@
+import dearpygui.dearpygui as dpg
+data_user = 'admin'
+data_password = 'password'
+dpg.create_context()
+with dpg.window(label='Login', no_collapse=True, no_resize=True, no_close=True, tag='login', show=True):
+	dpg.set_primary_window('login', True)
+	dpg.add_text('Username:')
+	username = dpg.add_input_text()
+	dpg.add_text('Password:')
+	password = dpg.add_input_text()
+	dpg.add_text('')
+	def login_func(user, passs):
+		iss = False
+		if user == data_user:
+			iss = True
+		if passs == data_password:
+			if iss == True:
+				dpg.configure_item('blank', default_value='Correct!')
+		else:
+			dpg.configure_item('blank', default_value='Incorrect credentials... Try again')
+			pass
+	dpg.add_button(label='Login', callback=lambda:login_func(user=dpg.get_value(username), passs=dpg.get_value(password)))
+	dpg.add_text('', tag='blank')
+dpg.create_viewport(title='Login System', width=835, height=697)
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
